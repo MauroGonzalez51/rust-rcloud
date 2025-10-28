89fe914bd66ee842f7e6270e9986fb53c110fc2e
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2025-10-28 18:27:34 -0500

    feat: implementing path_list function

--- src/cli/commands/path/handlers/list.rs
+++ src/cli/commands/path/handlers/list.rs
@@ -1,5 +1,26 @@
+use console::Style;
+
 use crate::{cli::parser::Args, config::prelude::*};
 
-pub fn path_list(_args: &Args, _registry: &Registry) {
-    todo!()
+pub fn path_list(_args: &Args, registry: &Registry) {
+    let idx_style = Style::new().bold().cyan();
+    let local_style = Style::new().green();
+    let arrow_style = Style::new().bold().yellow();
+    let remote_style = Style::new().blue();
+    let hooks_style = Style::new().bold().magenta();
+
+    for (i, path) in registry.paths.iter().enumerate() {
+        println!(
+            "{} {} {} {} {} {}",
+            idx_style.apply_to(format!("> {}.", i + 1)),
+            local_style.apply_to(&path.local_path),
+            arrow_style.apply_to("->"),
+            remote_style.apply_to(&path.remote_path),
+            hooks_style.apply_to(format!(
+                "[{} Hook(s)]",
+                path.hooks.pull.len() + path.hooks.push.len()
+            )),
+            Style::new().dim().apply_to(""),
+        );
+    }
 }
