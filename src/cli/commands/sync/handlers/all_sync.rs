1d355b62aba6f00a5e73ea97f3aabebb824b7b76
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2025-11-14 02:32:26 -0500

    fix: improve logging and error handling in all_sync function

--- src/cli/commands/sync/handlers/all_sync.rs
+++ src/cli/commands/sync/handlers/all_sync.rs
@@ -1,6 +1,5 @@
+use crate::{cli::parser::Args, config::prelude::*, log_debug, log_error, log_info, log_warn};
 use anyhow::Context;
-
-use crate::{cli::parser::Args, config::prelude::*, log_info};
 
 pub fn all_sync(
     args: &Args,
@@ -8,7 +7,7 @@ pub fn all_sync(
     tags: &[String],
     force_all: &bool,
 ) -> anyhow::Result<()> {
-    log_info!("{:?}", tags);
+    log_debug!("using tags: {:?}", tags);
 
     let matching_paths_ids: Vec<String> = match tags.is_empty() {
         true => registry.paths.iter().map(|p| p.id.clone()).collect(),
@@ -20,6 +19,8 @@ pub fn all_sync(
             .collect(),
     };
 
+    log_info!("found {} path(s) to sync", matching_paths_ids.len());
+
     for path_id in matching_paths_ids {
         let path_info = registry
             .paths
@@ -30,14 +31,35 @@ pub fn all_sync(
         if let Some((local_path, remote_path)) = path_info {
             log_info!("Sync path: {} -> {}", local_path, remote_path);
 
-            crate::cli::commands::sync::handlers::path_sync::path_sync(
+            match crate::cli::commands::sync::handlers::path_sync::path_sync(
                 args,
                 registry,
                 &None,
                 &Some(path_id),
                 force_all,
-            )
-            .with_context(|| format!("failed to sync path: {} -> {}", local_path, remote_path))?;
+            ) {
+                Ok(_) => {
+                    log_info!("synced {} -> {}", local_path, remote_path);
+                }
+                Err(err) => {
+                    log_error!(
+                        "an error ocurred while syncing {} -> {}: {}",
+                        local_path,
+                        remote_path,
+                        err
+                    );
+
+                    let should_continue = inquire::Confirm::new("continue?")
+                        .with_default(true)
+                        .prompt()
+                        .context("failed to get confirmation")?;
+
+                    if !should_continue {
+                        log_warn!("sync aborted by user");
+                        break;
+                    }
+                }
+            }
         }
     }
 
