27ee5679af56b96038f766e8020f082fee144c8d
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2025-12-01 00:53:23 -0500

    fix: getting remote replicas based on remote path configured on hook

--- src/hooks/backup/utils/rotate_remote_replicas.rs
+++ src/hooks/backup/utils/rotate_remote_replicas.rs
@@ -6,6 +6,7 @@ pub fn rotate_remote_replicas(
     max_replicas: usize,
     rclone_path: &str,
     remote_config: &Remote,
+    remote_backup_path: &str,
 ) -> anyhow::Result<()> {
     remote_replicas.sort_by(|a, b| b.timestamp.cmp(&a.timestamp));
 
@@ -15,29 +16,27 @@ pub fn rotate_remote_replicas(
         let to_remove = (current_count - max_replicas) + 1;
 
         for old in remote_replicas.iter().rev().take(to_remove) {
+            let filename = old
+                .path
+                .file_name()
+                .unwrap_or(old.path.as_os_str())
+                .to_string_lossy();
+
+            let remote_path = format!(
+                "{}:{}/{}",
+                remote_config.remote_name, remote_backup_path, filename
+            );
+
             let output = std::process::Command::new(rclone_path)
-                .args([
-                    "purge",
-                    &format!(
-                        "{}:{}",
-                        remote_config.remote_name,
-                        old.path.to_str().context("failed to convert path to str")?
-                    ),
-                ])
+                .args(["purge", &remote_path])
                 .output()
-                .with_context(|| {
-                    format!(
-                        "failed to purge remote file/dir: {}:{:?}",
-                        remote_config.remote_name, old.path
-                    )
-                })?;
+                .with_context(|| format!("failed to purge remote file/dir: {}", remote_path,))?;
 
             if !output.status.success() {
                 let stderr = String::from_utf8_lossy(&output.stderr);
                 log_warn!(
-                    "failed to purge remote file/dir: {}:{:?} ({})",
-                    remote_config.remote_name,
-                    old.path,
+                    "failed to purge remote file/dir: {} ({})",
+                    remote_path,
                     stderr
                 )
             }
